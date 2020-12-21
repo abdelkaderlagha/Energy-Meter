8b4340a0b8946e1bf6bb8645a27fcd89800f05c7
abdelkaderlagha <abdelkaderlaghaa@outlook.fr>
2020-12-21 16:28:38 +0100

    initial init

---
+++ src/main/java/gadour/springboot/energy/EnerrgyMeterApplication.java
@@ -0,0 +1,13 @@
+package gadour.springboot.energy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnerrgyMeterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnerrgyMeterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gadour/springboot/energy/EnerrgyMeterApplicationTests.java
@@ -0,0 +1,13 @@
+package gadour.springboot.energy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnerrgyMeterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
